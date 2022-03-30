7b78985a90548a5556fec7069f49f02b8fffb5a1
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2022-03-30 08:04:45 +0700

    Summary

--- app/src/main/java/rizkydwisaputra/id/muslimcoder/makananpantura/ui/auth/signin/SigninFragment.kt
+++ app/src/main/java/rizkydwisaputra/id/muslimcoder/makananpantura/ui/auth/signin/SigninFragment.kt
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.splash.*
 import rizkydwisaputra.id.muslimcoder.makananpantura.FoodMarket
 import rizkydwisaputra.id.muslimcoder.makananpantura.ui.MainActivity
 import rizkydwisaputra.id.muslimcoder.makananpantura.R
@@ -25,7 +28,7 @@ class SigninFragment : Fragment(), SigninContract.View {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         presenter = SigninPresenter(this)
-        if (!foodMarket.getApp().getToken().isNullOrEmpty()) {
+        if (!foodmarket.getApp().getToken().isNullOrEmpty()) {
             val home = Intent(activity, MainActivity::class.java)
             startActivity(home)
             activity?.finish()
@@ -59,6 +62,10 @@ class SigninFragment : Fragment(), SigninContract.View {
         val home = Intent(activity, MainActivity::class.java)
         startActivity(home)
         activity?.finish()
+    }
+
+    override fun onLoginFailes(message: String) {
+        TODO("Not yet implemented")
     }
 
     override fun override() {
